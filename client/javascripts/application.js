7d5d97f637f7730f57a294414ba465c19a33d8f2
Kenneth Lee <kennethkl@gmail.com>
2013-03-01 02:35:42 +0000

    we have a base

--- client/javascripts/application.js
+++ client/javascripts/application.js
@@ -1,5 +1,19 @@
+/*
 var socket = io.connect();
+
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { my: 'data' });
 });
+*/
+
+// Connect to server
+var socket = io.connect();
+
+// Listen for acknoledgement
+
+// Draw initial screens
+var context = document.querySelector('#game').getContext('2d');
+
+// Listen for more stuff and react
+// Listen for state
--- server.js
+++ server.js
@@ -38,8 +38,15 @@ function handler(request, response) {
 }
 
 io.sockets.on('connection', function (socket) {
+  /*
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
+  */
+  
+  // Send acknoledgement
+  // Synchronize state
+  
+  
 });
