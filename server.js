eb2d713ff3b93daae164d85140d69e939794e4a7
Kenneth Lee <kennethkl@gmail.com>
2013-03-01 02:05:29 +0000

    refactored script to javascript/application.js

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 var port = process.env.PORT || 8080;
 
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs');
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    url = require("url"),
+		path = require("path"),
+    fs = require('fs');
 
 app.listen(parseInt(port, 10));
-io.set('log level', 1);
+io.set('log level', 1);	// Debug
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/client/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
+function handler(request, response) {
+  var uri = url.parse(request.url).pathname,
+      filename = path.join(__dirname, 'client', uri);
+	fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
     }
 
-    res.writeHead(200);
-    res.end(data);
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
   });
 }
 
