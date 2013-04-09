c0765497b784962e0ac4740287158dabd5e7db99
Kenneth Lee <kennethkl@gmail.com>
2013-04-08 20:49:06 -0400

    added nodeunit to package.json and updated unit tests

--- server/deck.js
+++ server/deck.js
@@ -1,21 +1,29 @@
 var Deck = function() {
-	this.cards = [];
+  this.cards = [];
   
   this.reset();
   this.shuffle();
 };
 
 /**
- * Reset cards in deck
+ * Reset cards in deck and shuffles
  */
 Deck.prototype.reset = function() {
-  this.cards = require('./decks/standard');
-}
+  var standardCards = require('./decks/standard');
+  this.cards = standardCards.slice(0);
+  this.shuffle();
+};
 
 //+ Jonas Raoni Soares Silva
 //@ http://jsfromhell.com/array/shuffle [v1.0]
-Deck.prototye.shuffle = function() {
-	for(var j, x, i = this.cards.length; i; j = parseInt(Math.random() * i), x = this.cards[--i], this.cards[i] = this.cards[j], this.cards[j] = x);
+Deck.prototype.shuffle = function() {
+  for(var j, x, i = this.cards.length; i; j = parseInt(Math.random() * i), x = this.cards[--i], this.cards[i] = this.cards[j], this.cards[j] = x);
 }
 
-modules.exports = Deck;
+Deck.prototype.deal = function(howMany) {
+  var howMany = howMany || 1;
+
+  return this.cards.splice(0, howMany);
+}
+
+module.exports = Deck;
--- test/test-deck.js
+++ test/test-deck.js
@@ -1,5 +1,24 @@
 var Deck = require('../server/deck');
+var StandardDeck = require('../server/decks/standard');
 
-exports.testCreate = function() {
-	var deck = new Deck();
+exports.testDeck = function(test) {
+  var deck = new Deck();
+
+  test.equal(deck.cards.length, StandardDeck.length);
+
+  var cards1 = deck.cards.slice(0);
+  deck.shuffle();
+  test.notDeepEqual(deck.cards, cards1);
+  test.equal(deck.cards.length, cards1.length);
+
+  var cards2 = deck.cards.slice(0);
+  var dealt = deck.deal(20);
+  test.equal(deck.cards.length, cards2.length - 20);
+  
+  var cards3 = deck.cards.slice(0);
+  deck.reset();
+  test.equal(deck.cards.length, StandardDeck.length);
+  test.notDeepEqual(deck.cards, StandardDeck);
+
+  test.done();
 }
