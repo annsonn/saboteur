b7b956366778aaa7aca19786497264c43be7e7dc
Anson <anson.chung@gmail.com>
2013-04-25 02:26:30 +0000

    creating job rules

--- server/decks/standard-jobs.js
+++ server/decks/standard-jobs.js
@@ -1,4 +1,18 @@
 module.exports = [
-  'saboteur',
-  'gold-digger'
+  3: { 'saboteur': 1, 
+  'gold-digger': 3 },
+  4: { 'saboteur': 1, 
+  'gold-digger': 4 },
+  5: { 'saboteur': 2, 
+  'gold-digger': 4 },
+  6: { 'saboteur': 2, 
+  'gold-digger': 5 },
+  7: { 'saboteur': 3, 
+  'gold-digger': 5 },
+  8: { 'saboteur': 3, 
+  'gold-digger': 6 },
+  9: { 'saboteur': 3, 
+  'gold-digger': 7 },
+  10: { 'saboteur': 4, 
+  'gold-digger': 7 }  
 ];
