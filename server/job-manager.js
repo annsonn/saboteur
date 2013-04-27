669a2a3f7876818250c9cbbbf6f3067cfd5501c4
Anson <anson.chung@gmail.com>
2013-04-27 15:49:33 +0000

    making the job stack and taking shuffle from deck

--- server/job-manager.js
+++ server/job-manager.js
@@ -1,13 +1,30 @@
 var JobManager = function() {
-  this.jobs;  
+  this.numSaboteurs;  
+  this.numMiners;
+  this.jobs = [];
 };
 
-JobManager.prototype.getJobs = function(numPlayers) {
+JobManager.prototype.getNumJobs = function(numPlayers) {
   var jobRules = require('./decks/standard-jobs');
-  this.jobs = jobRules[numPlayers];
+  this.numSaboteurs = jobRules[numPlayers][0];
+  this.numMiners = jobRules[numPlayers][1];
 };
 
+JobManager.prototype.makeJobStack = function(){
+  for (var i = 0; i < numSaboteurs; i++) {
+    this.job = this.job.push['saboteur'];
+  }
+  
+  for (var i = 0; i < numMiners; i++) {
+    this.job = this.job.push['gold-digger'];
+  }
+}
 
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/array/shuffle [v1.0]
+JobManager.prototype.shuffle = function() {
+  for(var j, x, i = this.cards.length; i; j = parseInt(Math.random() * i), x = this.jobs[--i], this.jobs[i] = this.jobs[j], this.jobs[j] = x);
+}
 
 
 module.exports = JobManager;
--- test/test-jobmanager.js
+++ test/test-jobmanager.js
@@ -5,19 +5,19 @@ var JobRules = require('../server/decks/standard-jobs');
 exports.testGetJobsThreePlayers = function(test) {
   var jobManager = new JobManager();
     
-  jobManager.getJobs('3');
+  jobManager.getNumJobs('3');
   
-  test.equals(jobManager.jobs[0], '1');
-  test.equals(jobManager.jobs[1], '3');
+  test.equals(jobManager.numSaboteurs, '1');
+  test.equals(jobManager.numMiners, '3');
   test.done();
 }
 
 exports.testGetJobsTenPlayers = function(test) {
   var jobManager = new JobManager();
   
-  jobManager.getJobs('10');
+  jobManager.getNumJobs('10');
   
-  test.equals(jobManager.jobs[0], '4');
-  test.equals(jobManager.jobs[1], '7');
+  test.equals(jobManager.numSaboteurs, '4');
+  test.equals(jobManager.numMiners, '7');
   test.done();
 }
