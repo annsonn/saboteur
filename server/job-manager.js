574161236799d99ab2875e41dc144aad9166a13a
Anson <anson.chung@gmail.com>
2013-04-27 16:40:13 +0000

    unit test for shuffle

--- server/job-manager.js
+++ server/job-manager.js
@@ -11,19 +11,19 @@ JobManager.prototype.getNumJobs = function(numPlayers) {
 };
 
 JobManager.prototype.makeJobStack = function(){
-  for (var i = 0; i < numSaboteurs; i++) {
-    this.job = this.job.push['saboteur'];
+  for (var i = 0; i < this.numSaboteurs; i++) {
+    this.jobs = this.jobs.concat('saboteur');
   }
   
-  for (var i = 0; i < numMiners; i++) {
-    this.job = this.job.push['gold-digger'];
+  for (var i = 0; i < this.numMiners; i++) {
+    this.jobs = this.jobs.concat('gold-digger');
   }
 }
 
 //+ Jonas Raoni Soares Silva
 //@ http://jsfromhell.com/array/shuffle [v1.0]
 JobManager.prototype.shuffle = function() {
-  for(var j, x, i = this.cards.length; i; j = parseInt(Math.random() * i), x = this.jobs[--i], this.jobs[i] = this.jobs[j], this.jobs[j] = x);
+  for(var j, x, i = this.jobs.length; i; j = parseInt(Math.random() * i), x = this.jobs[--i], this.jobs[i] = this.jobs[j], this.jobs[j] = x);
 }
 
 
--- test/test-jobmanager.js
+++ test/test-jobmanager.js
@@ -21,3 +21,33 @@ exports.testGetJobsTenPlayers = function(test) {
   test.equals(jobManager.numMiners, '7');
   test.done();
 }
+
+
+exports.testMakeJobStackFivePlayers = function(test) {
+  var jobManager = new JobManager();
+  
+  jobManager.numSaboteurs = '2';
+  jobManager.numMiners = '4';
+  
+	jobManager.makeJobStack();
+  
+  test.equals(jobManager.jobs.length, '6');
+  test.done();
+}
+
+exports.testJobShuffle = function(test) {
+  var jobManager = new JobManager();
+  
+  jobManager.numSaboteurs = '3';
+  jobManager.numMiners = '5';
+  
+  jobManager.makeJobStack();
+  var originJobStack = jobManager.jobs.slice(0);
+  
+  jobManager.shuffle();
+  
+  test.equals(originJobStack.length, jobManager.jobs.length);  
+  test.notDeepEqual(jobManager.jobs, originJobStack);
+  
+  test.done();
+}
