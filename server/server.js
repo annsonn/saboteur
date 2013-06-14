cb054005ceb9a7e49a3c8e7c29ceb5ef0caf6252
kennethklee <kennethkl@gmail.com>
2013-06-13 20:22:32 -0400

    removed hard words and fixed player number duplication

--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ var Server = function() {
       
       joinGame: function(socket, callback) {
         return function(id) {
-          id = id.trim();
+          id = id.trim().toLowerCase();
           var game = self.games[id];
           if (!game) {
             console.log('Device tried to join non-existing game ' + id);
